7d91937f414c70a77234e4413ca0bdf98977dca9
PankajPrajapati <paprajapati.25.1@gmail.com>
2023-03-20 13:03:13 +0530

    update cards

--- src/components/carddata.js
+++ src/components/carddata.js
@@ -1,67 +1,64 @@
-
 import "../com.css";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import { MDBIcon } from "mdb-react-ui-kit";
-import { useSelector, useDispatch } from "react-redux";
-import { addUserThunk, deleteThunk, updateThunk } from "../slice/getdataslice";
+import { useDispatch } from "react-redux";
+import { deleteThunk, updateThunk } from "../slice/getdataslice";
 import * as Yup from "yup";
 import { useFormik } from "formik";
 
+const CardData = ({ data }) => {
+  const dispatch = useDispatch();
+  const [val, setVal] = useState(true);
 
-const CardData = ({data}) => {
+  console.log("ID IN CARD DATA:",data.id)
 
- const dispatch = useDispatch();
- const [val, setVal] = useState(true);
+  const formik = useFormik({
+    initialValues: {
+      id:"",
+      name: "",
+      email: "",
+      author: "",
+    },
+    onSubmit: (values) => {
+      console.log("values", values);
+      dispatch(updateThunk(values))
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .min(2, "Name must be more than 1 charecter")
+        .max(15, "Name must be 15 charecter or less")
+        .required("Required"),
+      email: Yup.string().email("Invalid Email Address").required("Required"),
+      author: Yup.string()
+        .max(20, "Author name must have 20 or less charecter")
+        .required("required"),
+    }),
+  });
 
-     const formik = useFormik({
-       initialValues: {
-         name: "",
-         email: "",
-         author: "",
-       },
-       onSubmit: (values) => {
-         console.log("values", values);
+  const updt = (data) => {
+    const id = data.id;
+    console.log("id when edit data", id);
 
-         values?.id
-           ? dispatch(updateThunk(values))
-           : dispatch(addUserThunk(values));
-       },
-       validationSchema: Yup.object({
-         name: Yup.string()
-           .min(2, "Name must be more than 1 charecter")
-           .max(15, "Name must be 15 charecter or less")
-           .required("Required"),
-         email: Yup.string()
-           .email("Invalid Email Address")
-           .required("Required"),
-         author: Yup.string()
-           .max(20, "Author name must have 20 or less charecter")
-           .required("required"),
-       }),
-     });
+    id === data.id
+      ? setVal(false)(
+          formik.setValues({
+            id:data.id,
+            name: data.name,
+            email: data.email,
+            author: data.author,
+          })
+        )
+      : setVal(true);
+  };
 
-     const updt = (data) => {
-       const id = data.id;
-       console.log("id when edit data", id);
 
-       id === data.id
-         ? setVal(false)(
-             formik.setValues({
-               name: data.name,
-               email: data.email,
-               author: data.author,
-             })
-           )
-         : setVal(true);
+ 
 
-       // dispatch(updateThunk(data));
-     };
-
-     const del = (id) => {
-       console.log("id in del method ", id);
-       dispatch(deleteThunk(id));
-     };
+  const del = (id) => {
+    console.log("id in del method ", id);
+    dispatch(deleteThunk(id));
+  };
 
   return (
     <div className="col-sm-2 col-lg-3" style={{ margin: 10 }} key={data.id}>
@@ -121,11 +118,49 @@ const CardData = ({data}) => {
               onChange={formik.handleChange}
               value={formik.values.author}
             />
+            <br />
+            {val === false ? (
+              <div>
+                <button type="submit">
+                  <MDBIcon fas icon="check" />
+                </button>
+                
+                <button type="submit">
+               <MDBIcon fas icon="times-circle" />
+              
+                </button>
+              </div>
+            ) : (
+              <button onClick={() => updt(data)}>
+                <MDBIcon far icon="edit" className="text-info" />
+              </button>
+            )}
           </form>
         )}
       </Card>
     </div>
   );
-}
+};
 
 export default CardData;
+
+//For Update
+// const upDate=(data)=>{
+//   console.log("Update Data through Val:",data)
+//   setUpdata(data)
+//    dispatch(updateThunk(data));
+// }
+
+// const handleUpdate = (data) => {
+//   const id = data.id;
+
+//   console.log("Id in handleUpdate:", id);
+//   console.log("Id of initial Data:");
+// };
+
+
+  // const handleUpdate = (data) => {
+  //   console.log("DATA ID:", data.id);
+  //   console.log("DATA IN HANDLEUPDATE:", data);
+  //   dispatch(updateThunk(data));
+  // };
--- src/components/cards.js
+++ src/components/cards.js
@@ -1,16 +1,8 @@
 import "../com.css";
-import React, { useEffect, useState } from "react";
-import Card from "react-bootstrap/Card";
-import { MDBIcon } from "mdb-react-ui-kit";
-import { useSelector, useDispatch } from "react-redux";
-import { addUserThunk, deleteThunk, updateThunk } from "../slice/getdataslice";
-import * as Yup from "yup";
-import { useFormik } from "formik";
+import { useSelector} from "react-redux";
 import CardData from "./carddata";
 
 const Cards = () => {
-  const dispatch = useDispatch();
-  const [val, setVal] = useState(true);
 
   const {
     isLoading,
@@ -18,71 +10,12 @@ const Cards = () => {
     allData: userData,
   } = useSelector((state) => state.userdata);
 
-  //   useEffect(() => {
-  //     //dispatch(thunkUserData());
-  //   });
-
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      author: "",
-    },
-    onSubmit: (values) => {
-      console.log("values", values);
-
-      values?.id
-        ? dispatch(updateThunk(values))
-        : dispatch(addUserThunk(values));
-    },
-    validationSchema: Yup.object({
-      name: Yup.string()
-        .min(2, "Name must be more than 1 charecter")
-        .max(15, "Name must be 15 charecter or less")
-        .required("Required"),
-      email: Yup.string().email("Invalid Email Address").required("Required"),
-      author: Yup.string()
-        .max(20, "Author name must have 20 or less charecter")
-        .required("required"),
-    }),
-  });
-
-   
-
-  const updt = (data) => {
-    const id = data.id;
-    console.log("id when edit data", id);
-        
-    
-
-     id === data.id
-       ? (setVal(false))
-         (formik.setValues(
-           {
-             name: data.name,
-             email: data.email,
-             author: data.author,
-           }))
-       : setVal(true);
-    
-   
-    // dispatch(updateThunk(data));
-  };
-
-  const del = (id) => {
-    console.log("id in del method ", id);
-    dispatch(deleteThunk(id));
-  };
-
   return (
     <>
       {isLoading && <div>Loading....</div>}
 
       <div class="row" style={{ marginLeft: 200 }}>
-        {userData &&
-          userData.map((data) => (
-            <CardData data={data}/>
-          ))}
+        {userData && userData.map((data) => <CardData data={data} />)}
       </div>
       {hasError && <div>Something went wrong...</div>}
     </>
